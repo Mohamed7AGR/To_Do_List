4390934130c22ae3627fd674509934eaab41d277
Mohamed7AGR <mohamed7ager222@gmail.com>
2025-04-22 14:57:23 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ let addBtn = document.querySelector("#addBtn");
 let taskInput = document.querySelector("#taskInput");
 let mySelect = document.querySelector("#mySelect");
 let searchInput = document.querySelector("#searchInput");
-let delBtn = document.querySelector("#delBtn");
 
 let todos = [];
 if (localStorage.getItem("allToDos") !== null) {
@@ -68,15 +67,7 @@ function clear() {
   taskInput.blur();
 }
 
-delBtn.addEventListener("click", function () {
-  deleteAll();
-});
 
-function deleteAll() {
-  todos = [];
-  localStorage.setItem("allToDos", JSON.stringify(todos));
-  filterTasks(mySelect.value);
-}
 function deleteToDo(id) {
   todos = todos.filter((task) => task.id !== id);
   localStorage.setItem("allToDos", JSON.stringify(todos));
